be5dfe5ac3a52f81eaf15bccc27f4b2d4f68955a
axell-brendow <breno.axel@gmail.com>
2021-09-12 20:15:44 -0300

    Start product catalog project

---
+++ product-catalog/src/main/java/br/com/axellbrendow/productcatalog/ProductCatalogApplication.java
@@ -0,0 +1,13 @@
+package br.com.axellbrendow.productcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCatalogApplication.class, args);
+	}
+
+}
