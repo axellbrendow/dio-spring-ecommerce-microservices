7431fb0c9ba770d3bba126e2e4366fca7c8342a1
axell-brendow <breno.axel@gmail.com>
2021-09-12 22:35:25 -0300

    Create Redis configuration

---
+++ shopping-cart/src/main/java/br/com/axellbrendow/shoppingcart/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package br.com.axellbrendow.shoppingcart.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+    @Value("${redis.host}")
+    private String host;
+
+    @Value("${redis.port}")
+    private int port;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        var config = new RedisStandaloneConfiguration(host, port);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        var template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
